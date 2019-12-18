e29c06c520bf2bafe3ecf1bba672e4263474289b
quzq <quzyam@gmail.com>
2019-12-18 12:57:16 +0900

    add sample.pdf

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello!')
+const fs = require('fs');
+const pdf = require('pdf-parse');
+
+const pdfPath = './input/jicfs.pdf'
+
+
+let dataBuffer = fs.readFileSync(pdfPath);
+ 
+pdf(dataBuffer).then(function(data) {
+ 
+    // number of pages
+    console.log(data.numpages);
+    // number of rendered pages
+    console.log(data.numrender);
+    // PDF info
+    console.log(data.info);
+    // PDF metadata
+    console.log(data.metadata); 
+    // PDF.js version
+    // check https://mozilla.github.io/pdf.js/getting_started/
+    console.log(data.version);
+    // PDF text
+    console.log(data.text); 
+        
+});
