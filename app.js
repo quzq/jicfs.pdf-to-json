baf8f6c22d5c9dcb3e2a12b35d62c328bd3271a7
quzq <quzyam@gmail.com>
2020-02-20 02:04:26 +0000

    bugfix: lost rows

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import * as _ from 'lodash';
 
 const pdfPath = './input/jicfs.pdf'
 export const parseRow = s => {
-  const matches = s.match(/(\d+)\s*([^ -~｡-ﾟ]+)\s*((?:[ｦ-ﾟ]|,)+)\s*([^ -~｡-ﾟ]+)/)
+  const matches = s.match(/(\d+)\s*([^ -~｡-ﾟ]+)\s*((?:[\-\(\)\s\.､･%ｦ-ﾟ0-9A-Z]|,)+)\s*([^ -~｡-ﾟ]+)/)
   return _.drop(matches)
 }
 
@@ -17,12 +17,13 @@ const main = async () => {
   fs.writeFileSync('./output/jicfs.txt', data.text, { mode: 0o755 });
   const lines = data.text.split(/\n/)
   const json = _.chain(lines)
+    .map(l => l.startsWith('JICFS分類') ? '' : l)
     .map(l => parseRow(l))
     .filter(l => l.length > 0)
     .map(l => ({
       [jsonKeys[0]]: l[0],
       [jsonKeys[1]]: l[1],
-      [jsonKeys[2]]: l[2],
+      [jsonKeys[2]]: _.trim(l[2]),
       [jsonKeys[3]]: l[3],
     })).value()
   const jsonString = '[' + _.reduce(json, (lines, l) => lines + `${JSON.stringify(l)},\n`, '') + ']'
